f5b324d194746641a2dbb2c629b4fac40dbff9ea
Chuyang Chen <chuyangchen2018@outlook.com>
2023-06-12 11:51:53 +0800

    Write a prototype for incremental backup functionality

--- chup/sigvault.py
+++ chup/sigvault.py
@@ -1,9 +1,8 @@
 from typing import TextIO, Optional
 import base64
 import pyrsync
-from dirtools import Dir
 import tarfile
-from tempfile import NamedTemporaryFile
+from tempfile import TemporaryFile, NamedTemporaryFile, TemporaryDirectory
 import os
 
 
@@ -16,25 +15,21 @@ class SigVaultWriter(object):
 
     def add(self, file_to_sig: str):
         with open(os.path.join(self.base_path, file_to_sig), 'rb') as file_to_sig:
-            with NamedTemporaryFile(delete=False) as f:
+            with TemporaryFile('w+b') as f:
                 pyrsync.signature(file_to_sig, f, 4, pyrsync.RS_RK_BLAKE2_SIG_MAGIC)
-                tmp_sig_file_path = f.name
-            with open(tmp_sig_file_path, 'rb') as tmp_sig_file:
-                self.__sig_file.write(f'{file_to_sig.name} {base64.encodebytes(tmp_sig_file.read()).decode("ascii")}')
-            os.remove(tmp_sig_file_path)
+                f.seek(0)
+                self.__sig_file.write(f'{os.path.relpath(file_to_sig.name, self.base_path)} {base64.encodebytes(f.read()).decode("ascii")}')
 
     def close(self):
-        self.__sig_file.close()
-        with open(self.__sig_file.name, 'rb') as sig_file:
-            sig_size = os.fstat(sig_file.fileno()).st_size
-            sig_info = tarfile.TarInfo(f'{self.base_path.strip("/")}/.sig')
-            sig_info.size = sig_size
-            self.tar.addfile(tarinfo=sig_info, fileobj=sig_file)
+        self.__sig_file.seek(0)
+        sig_size = os.fstat(self.__sig_file.fileno()).st_size
+        # self.tar.addfile(tarinfo=sig_info, fileobj=self.__sig_file)
+        self.tar.add(self.__sig_file.name, arcname='.sig')
         self.tar.close()
-        os.remove(self.__sig_file.name)
+        self.__sig_file.close()
 
     def __enter__(self):
-        self.__sig_file = NamedTemporaryFile(delete=False, mode='wt')
+        self.__sig_file = NamedTemporaryFile(mode='w+t')
         self.tar = tarfile.open(name=self.__tar_ball_path, mode='a')
         return self
 
@@ -43,28 +38,29 @@ class SigVaultWriter(object):
 
 
 class SigVaultReader(object):
-    def __init__(self, base_path='.', key=None):
-        self.tars = []
-        _dir = Dir(base_path)
-        for sv_file in _dir.files('{0}.sigvault.*.tgz'.format(key),
-                                  sort_reverse=True):
-            archive = open(os.path.join(_dir.path, sv_file), 'rb')
-            tar = tarfile.open(fileobj=archive, mode='r:gz')
-            self.tars.append(tar)
+    def __init__(self, tarball_path: str):
+        self.__tarball_path = tarball_path
+        self.tar: Optional[tarfile.TarFile] = None
 
     def extract(self, path):
-        for tar in self.tars:
-            try:
-                m = tar.getmember(path)
-                return tar.extractfile(m)
-            except:
-                pass
+        self.tar.extract(os.path.join('data', path))
+
+    @property
+    def sigs(self) -> dict[str, bytes]:
+        sigs = {}
+        with TemporaryDirectory('w+') as p:
+            self.tar.extract('.sig', p)
+            with open(os.path.join(p, '.sig'), 'r') as sig_file:
+                for line in sig_file:
+                    sigs[line.split(' ')[0]] = base64.decodebytes(line.split(' ')[1].encode('ascii'))
+        return sigs
 
     def __enter__(self):
+        self.tar = tarfile.open(name=self.__tarball_path, mode='r')
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+        self.tar.close()
 
 
 class SigVault(object):
